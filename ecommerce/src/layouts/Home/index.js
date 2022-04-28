025038bf15957d65272d0be401aaa43ad18718eb
Reza <rdalvand@yahoo.com>
2022-04-28 16:30:46 +0430

    ..

--- ecommerce/src/layouts/Home/index.js
+++ ecommerce/src/layouts/Home/index.js
@@ -4,32 +4,9 @@ import MainSlider from "../../PlatForm/slider";
 import ProductCard from "../../components/ProductCard";
 import { Box, Typography } from "@mui/material";
 import useStyle from "./style";
-import Slider from "react-slick";
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import ProductItem from "../../components/ProductItem";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
-const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 4,
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-  },
-};
 function Home() {
   const classes = useStyle();
   const [showNewProdcuts, setShowNew] = useState(true);
@@ -42,41 +19,6 @@ function Home() {
   const showBestProductHandler = () => {
     setShowNew(false);
     setShowBest(true);
-  };
-
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
   };
 
   return (
